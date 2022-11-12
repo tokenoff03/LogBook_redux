2457d1c34931b0c25541100be7cf679135e5cfea
Adil <v-i-p.adil@mail.ru>
2022-11-12 22:23:04 +0600

    localStorage

--- src/components/RegSignIn/RegSignIn.jsx
+++ src/components/RegSignIn/RegSignIn.jsx
@@ -1,8 +1,31 @@
 import rs from "./RegSignIn.module.css";
 import ns from "../Nav/Nav.module.css";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
 
 function RegSignIn() {
+  const [password, setPassword] = useState();
+  const [login, setLogin] = useState();
+  const checkUser = (usersArray, existLogin, existPassword) => {
+    for (let i = 0; i < usersArray.length; i++) {
+      if (
+        usersArray[i].login == existLogin &&
+        usersArray[i].password == existPassword
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const clickButton = () => {
+    let log_info = JSON.parse(localStorage.getItem("logInfo"));
+    if (localStorage.getItem("logInfo")) {
+      if (checkUser(log_info.users, login, password)) alert("Welcome " + login);
+      else alert("Incorrect password or User does not exist");
+    } else alert("Incorrect password or User does not exist");
+  };
+
   return (
     <div className={rs.Registration}>
       <div className={rs.signInContainer}>
@@ -10,12 +33,21 @@ function RegSignIn() {
           <h1 className={rs.title}>LogBook</h1>
         </div>
         <div className={rs.inputData}>
-          <input placeholder={"Имя пользователя или эл. адрес"} />
-          <input placeholder={"Пароль"} />
+          <input
+            placeholder={"Имя пользователя или эл. адрес"}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder={"Пароль"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <div className={rs.signInButton}>
-          <span>Войти</span>
-        </div>
+        <NavLink to="/">
+          <button className={rs.signInButton} onClick={clickButton}>
+            Войти
+          </button>
+        </NavLink>
         <div className={rs.helpButtons}>
           <span>Забыли пароль?</span>
         </div>
@@ -25,9 +57,11 @@ function RegSignIn() {
           <p>
             У вас нет аккаунта?
             <NavLink
-                to="/sign-up"
-                className={(navData) => (navData.isActive ? ns.active : "")}
-            > Зарегистрируйтесь
+              to="/sign-up"
+              className={(navData) => (navData.isActive ? ns.active : "")}
+            >
+              {" "}
+              Зарегистрируйтесь
             </NavLink>
           </p>
         </div>
--- src/components/RegSignUp/RegSignUp.jsx
+++ src/components/RegSignUp/RegSignUp.jsx
@@ -1,8 +1,38 @@
 import rs from "./RegSignUp.module.css";
 import ns from "../Nav/Nav.module.css";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
 
 function RegSignUp() {
+  const [password, setPassword] = useState();
+  const [login, setLogin] = useState();
+  const checkUser = (usersArray, existLogin, existPassword) => {
+    for (let i = 0; i < usersArray.length; i++) {
+      if (
+        usersArray[i].login == existLogin &&
+        usersArray[i].password == existPassword
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  function clickButton(event) {
+    if (localStorage.getItem("logInfo")) {
+      let log_info = JSON.parse(localStorage.getItem("logInfo"));
+      if (checkUser(log_info.users, login, password)) {
+        alert("User Exist!!");
+      } else {
+        log_info.users.push({ login: login, password: password });
+        localStorage.setItem("logInfo", JSON.stringify(log_info));
+      }
+    } else {
+      let log_info = [{ login: login, password: password }];
+      localStorage.setItem("logInfo", JSON.stringify({ users: log_info }));
+    }
+  }
+
   return (
     <div className={rs.Registration}>
       <div className={rs.signUpContainer}>
@@ -13,28 +43,44 @@ function RegSignUp() {
           <h3>Зарегистрируйтесь, чтобы увидеть фото и посты ваших друзей</h3>
         </div>
         <div className={rs.inputData}>
-          <input placeholder={"Электронный адрес"}/>
-          <input placeholder={"Имя и фамилия"}/>
-          <input placeholder={"Имя пользователя"}/>
-          <input placeholder={"Пароль"}/>
+          <input placeholder={"Электронный адрес"} type="email" />
+          <input placeholder={"Имя и фамилия"} />
+          <input
+            placeholder={"Имя пользователя"}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder={"Пароль"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className={rs.privacyPolicy}>
           <p>
-            Регистрируясь, вы принимаете наши <b>Условия, Политику конфедициональности</b> и <b>Политику в отношении файлов cookie</b>.
+            Регистрируясь, вы принимаете наши{" "}
+            <b>Условия, Политику конфедициональности</b> и{" "}
+            <b>Политику в отношении файлов cookie</b>.
           </p>
         </div>
-        <div className={rs.signUpButton}>
-          <span>Регистрация</span>
-        </div>
+        <NavLink to="/sign-in">
+          <button
+            className={rs.signUpButton}
+            onClick={(event) => clickButton(event)}
+          >
+            Регистрация
+          </button>
+        </NavLink>
       </div>
       <div className={rs.signInContainer}>
         <div className={rs.signInLink}>
           <p>
             Есть аккаунт?
             <NavLink
-                to="/sign-in"
-                className={(navData) => (navData.isActive ? ns.active : "")}
-            > Войти
+              to="/sign-in"
+              className={(navData) => (navData.isActive ? ns.active : "")}
+            >
+              {" "}
+              Войти
             </NavLink>
           </p>
         </div>
