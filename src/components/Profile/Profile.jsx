72dd260281cc44a0f16fbe97ec6c1200085f9dab
Adil <v-i-p.adil@mail.ru>
2022-11-16 13:50:47 +0600

    Profile changed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,46 @@ import ps from "./Profile.module.css";
 function Profile() {
   return (
     <div className={ps.Profile}>
-      <div class="container">asdsad</div>
+        <div className={ps.container}>
+            <div className={ps.header}>
+                <div className={ps.headerLeft}>
+                    <img className={ps.headerLeftLogo} width={150} height={150} src="/img/logo.png"/>
+                </div>
+                <div className={ps.headerRight}>
+                    <div className={ps.ProfileName}>
+                        <h2>aaskhat.k.m_</h2>
+
+                        {/*<a className={ps.ProfileEdit}>Редактировать профиль</a>*/}
+                        {/*<img className={ps.ProfileSetting} width={24} height={24} src="/img/settings.png"/>*/}
+
+                    </div>
+
+                    <ul className={ps.ProfileFriends}>
+                        <li className={ps.ProfileFriend}>
+                            <span>3</span> пубкликаций
+                        </li>
+                        <li className={ps.ProfileFriend}>
+                            <span>168</span> подписчиков
+                        </li>
+                        <li className={ps.ProfileFriend}>
+                            <span>206</span> друзья
+                        </li>
+                    </ul>
+
+                    <div className={ps.ProfileInformation}>
+                        <span>Askhvt_Kaym kz</span>
+                        <br/>
+                        <div>
+                            IITU'24
+                            <br/>
+                            19 y.o.
+                            <br/>
+                            Личная жизнь, не выставляемая на показ, приносит столько радости. Словно это и есть настоящая жизнь.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
